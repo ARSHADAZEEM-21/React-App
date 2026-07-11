8b5fe31f49163ce0feff117a399e40957030229b
Arshad Azeem <Arshadazeem2121@gmail.com>
2026-07-11 16:48:26 +0530

    fetch using button

--- DailyEats/src/components/Body.jsx
+++ DailyEats/src/components/Body.jsx
@@ -3,10 +3,22 @@ import { restaurantsArr } from "../utils/mockData";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+
+    const getData = async () => {
+        const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.374638644228302&lng=78.4300148114562&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",);
+
+        const data = await response.json();
+        console.log(data);
+    }
     return (
         <div className="body">
             <h3 className="res-title">Top Restaurants in Hyderabad</h3>
-            <Shimmer/>
+            {/* <Shimmer/> */}
+            <div style={{margin:"100px"}}>
+                {""}
+                <button onClick={getData}>GetData</button>
+            </div>
+
             <div className="res-container">
                 {/* {restaurantsArr.map((resObj) => {
                     return <RestaurantCard resDetail={resObj} key= {resObj.id} />;
